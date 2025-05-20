409a7a7e4ccdfa5939b8dcb792eb6e0ed81eedfd
pc <fahlaouimohammed@gmail.com>
2025-05-20 03:25:37 +0100

    patch-routing-profile-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AccountComponent} from './Account/account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"account", component:AccountComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

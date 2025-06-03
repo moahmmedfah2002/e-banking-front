0f502fbfff7d7dcd66e0dbf795212fb7365726e8
gold <m.soukrati7242@uca.ac.ma>
2025-06-03 01:59:32 +0100

    adding more functionality to admin and user

---
+++ src/app/agent/agent-layout/agent-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-layout',
+  standalone: false,
+  templateUrl: './agent-layout.component.html',
+  styleUrl: './agent-layout.component.css'
+})
+export class AgentLayoutComponent {
+
+}

b978b63f61548313eec98fedca8fbb4f753ea62b
fahlaoui <fahlaouimohammed@gmail.com>
2025-06-03 01:56:58 +0100

    HOME-LINK

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -10,25 +10,25 @@ import { Compte } from '../../modele/Compte';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent{
+export class HomePageComponent implements OnInit{
   public user:User=new User();
   public homeService:HomeService=inject(HomeService);
   public router:Router=inject(Router);
-  
-  // ngOnInit(): void {
-  //   let token=sessionStorage.getItem('authToken');
-  //   if(token==null){
-  //     this.router.navigate(['/login']);
 
-  //   }else{
-  //    this.homeService.getUser(token).subscribe(e=>{
-  //      this.user=e;
+  ngOnInit(): void {
+    let token=sessionStorage.getItem('authToken');
+    if(token==null){
+      this.router.navigate(['/login']);
 
-  //    })
-  //   }
+    }else{
+     this.homeService.getUser(token).subscribe(e=>{
+       this.user=e;
+
+     })
+    }
 
 
-  // }
+  }
 
 
 }

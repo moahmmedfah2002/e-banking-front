28cf0b11d1a89c4b99a3624cbc9e6132ecfb64bc
fahlaoui <fahlaouimohammed@gmail.com>
2025-06-10 20:11:10 +0100

    ci/cd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://13.51.109.196:8082" // Set to null to use mock data in development
+  apiUrl: "http://51.20.31.251:8082" // Set to null to use mock data in development
 };
